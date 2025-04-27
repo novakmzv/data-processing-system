03e168f18aa7bfc5f8723c2adee802091c7289d6
novak <novakmzv@gmail.com>
2025-04-27 15:49:06 -0500

    feat: Configure ThrottlerModule with 1 request per minute limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+imports: [
+     ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 1,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
